509a4d592a8039feca6ba3bfbacaf35735f53320
Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar>
2023-06-01 12:41:17 -0300

    sync y tareas

--- main/main.c
+++ main/main.c
@@ -1,78 +1,55 @@
+/* main.c: programa ejemplo con tres tareas concurrentes.
+ *
+ * tarea_led: prioridad 20, pila 64 bytes.
+ * tarea_comunicaciones: prioridad 20, pila 256 bytes
+ * tarea_main: prioridad 20, pila 256 bytes
+ *
+ * dos semaforos para sincronizar una alternancia de uso del UART
+ */
+
 #include <xinu.h>
 #include <avr/interrupt.h>
 #include "serial.h"
 #include "sync.h"
 
+/* definicion de tareas (cuyo codigo fuente es externo) */
+extern int led_placa(void);
+extern int comunicacion(void);
 
+/* nombre para dos semaforos a utilizar */
 #define SEM_COM 0
 #define SEM_PROD 1
 
-int comunicacion(void)
-{
-	while(1) {
-		sync_wait(SEM_PROD);
-		serial_put_str("Arriba america!\n");
-		sync_signal(SEM_COM);
-		
-		sleep(2);
-	}
 
-}
-
-
-int led_placa(void)
-{
-    volatile unsigned char* DDR_B = (unsigned char*) 0x24;
-    volatile unsigned char* PUERTO_B = (unsigned char*) 0x25;
-
-    *DDR_B = 0x20; // control: salida.
-
-    while (1){
-        sleepms(500);
-        *PUERTO_B = 0x20; // high
-        sleepms(500);
-        *PUERTO_B = 0; // low
-    }
-
-}
-
-void freemem_get(void)
-{
-	struct  memblk  *memptr;        /* Ptr to memory block          */
-        uint32  free_mem;               /* Total amount of free memory  */
-
-        /* Output Xinu memory layout */
-
-        free_mem = 0;
-        for (memptr = memlist.mnext; memptr != NULL;
-					memptr = memptr->mnext) {
-                free_mem += memptr->mlength;
-        }
-        kprintf("\nFreeMEM:%d (bytes)\n\n", free_mem);
-}
-
-
+/* main es una tarea independiente y se la utiliza como tal */
 int main(void)
 {
 
 	serial_init();
 
+	/* inicializamos los semaforos */
+	sync_set(SEM_COM, 1);
+	sync_set(SEM_PROD, 0);
 
+	/* creamos y ponemos a ejecutar dos tareas */
 	resume(create(led_placa, 64, 10, "led", 0));
 	resume(create(comunicacion, 256, 20, "comm", 0));
 
-	sync_set(SEM_COM, 1);
-	sync_set(SEM_PROD, 0);
 
 	while(1) {
 		sleep(1);
 
+                /* como esta tarea comparte el serial con otra tarea, 
+                 * se lo sincroniza estilo productor-consumidor,
+                 * para que cada uno envie un mensaje de manera
+                 * alternada. Un mutex podría haber sido utilizado
+		 * si no es necesaria la alternancia.
+                 */
+
 		sync_wait(SEM_COM);
-		//kprintf("hola mundo\n");
-		serial_put_str("hola mundo\n");
+			serial_put_str("hola mundo\n");
 		sync_signal(SEM_PROD);
 		
-//		freemem_get();
 		
 	}
 
